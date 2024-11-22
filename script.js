eb9c5e79bc3ec6a9932aa8d4f3fedfa45ec24672
Asifnawaz89 <malikasifnawaz1989@gmail.com>
2024-11-22 11:16:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let popup = "Error! Please enter a valid password."
+alert(popup) 
+let welcome = "Welcome to JS land \n Happy Coding!" 
+alert(welcome)
+let happy = "Happy Coding \n Prevent this page from creating additional pages."
+alert(happy)
+let age = "30 years old"
+alert(age)
+let certify = "Certified Mobile Application Development"
+alert(certify)
+let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+let email = "My email address is asif@gmail.com"
+alert(email)
+let learn = "I am trying to learn from the book.A smarter way to learn JS."
+alert(learn)
+let times ="You visited this site 10 times."
+alert(times)
+let order =" John doe oredred 7 t-sirts."
+alert(order)
+let shirt ="Black Hoodie"
+alert(shirt)
+let price = " $12"
+alert(price)
+let greet = "Thank You For Shopping"
+alert(greet)
